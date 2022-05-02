0700ba91c73b3b5e99bc1e3874036b4f20c3504e
TaoMaru <jackson_maria1@columbusstate.edu>
2022-05-02 18:41:18 -0400

    CookBook with all menu options, almost complete

--- CookBookDriver.java
+++ CookBookDriver.java
@@ -16,6 +16,8 @@
  * @version 1.4 Apr 28, 2022 - added menu options for saving/loading recipe files & their
  * respective methods, added menu option for merging loaded recipes with new recipes,
  * modulated menu options 1-7 more clearly.
+ * @version 1.5 May 02, 2022 - added methods for searching recipes and deleting recipes,
+ * added setting ingredients functionality to creating new recipe with submenus
  */
 
 import java.util.ArrayList;
@@ -44,6 +46,8 @@ public class CookBookDriver implements Serializable
         + "\nB - Baking Recipe"
         + "\nC - Cooking Recipe";
     private String validRecipeTypes = "BC";
+    private String yesNoChoice;
+    private String validYNOptions = "YN";
     private String typeChoice;
     private Scanner inScan;
     private boolean invalid;
@@ -104,10 +108,14 @@ public class CookBookDriver implements Serializable
         do
         {
             displayMenu();
-            System.out.println("Please select from the menu: ");
+            System.out.print("Please select from the menu: ");
             if(inScan.hasNextLine())
             {
                 menuChoice = inScan.nextLine();
+                if(menuChoice.equals(""))
+                {
+                    menuChoice = inScan.nextLine();
+                }
                 if(menuChoice.length() == 1 && validOptions.contains(menuChoice))
                 {
                     invalid = false;
@@ -132,7 +140,7 @@ public class CookBookDriver implements Serializable
         do
         {
             displayRecipeTypeOptions();
-            System.out.println("Please choose a recipe type: ");
+            System.out.print(" \n " + "Please choose a recipe type: ");
             if(inScan.hasNextLine())
             {
                 typeChoice = inScan.nextLine().toUpperCase();
@@ -148,6 +156,86 @@ public class CookBookDriver implements Serializable
         }
         while(invalid);
         return typeChoice;
+    }
+    
+    /**get yes/no choice for adding ingredients to new recipe
+     * @return boolean - true if Yes chosen, i.e. an ingredient will be added
+     */
+    public boolean addingIngredient()
+    {
+        invalid = true;
+        yesNoChoice = "";
+        do
+        {
+            System.out.print(" \n" + "Would you like to add an ingredient (Y/N): ");
+            if(inScan.hasNextLine())
+            {
+                yesNoChoice = inScan.nextLine();
+            }
+            if(yesNoChoice.equals(""))
+            {
+                yesNoChoice = inScan.nextLine();
+            }
+            if(yesNoChoice.length() == 1 && validYNOptions.contains
+                                                (yesNoChoice.toUpperCase()))
+            {
+                invalid = false;
+                if(yesNoChoice.toUpperCase().equals("Y"))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                System.out.println(yesNoChoice + " is not a valid option");
+            }
+        }
+        while(invalid);
+        return false;
+    }
+    
+    /** get ingredient name and amount for new recipe, set new ingredient
+     * 
+     */
+    public void addIngredients()
+    {
+        while(addingIngredient())
+        {
+            String ingredientName = "";
+            Double ingredientAmount = 0.0;
+            System.out.print("Please enter the ingredient name: ");
+            if(inScan.hasNextLine())
+            {
+                ingredientName = inScan.nextLine();
+            }
+            System.out.print("Please enter the ingredient amount: ");
+            if(inScan.hasNextDouble())
+            {
+                ingredientAmount = inScan.nextDouble();
+            }
+            System.out.println("Adding new ingredient: " + ingredientName 
+                + " " + ingredientAmount);
+            newRecipe.addNewIngredient(ingredientName, ingredientAmount);
+        }
+    }
+    
+    /** set cook time for new recipe
+     * 
+     */
+    public void setCookTime()
+    {
+        Double cookTime = 0.0;
+        String cookType = "bake";
+        if(typeChoice.equals("C"))
+        {
+            cookType = "cook";
+        }
+        System.out.print(" \n" + "Please enter the " + cookType + " time: ");
+        if(inScan.hasNextDouble())
+        {
+            cookTime = inScan.nextDouble();
+        }
+        newRecipe.setCookTime(cookTime);
     }
     
     //recipe creation, deletion, and editing methods:
@@ -190,7 +278,7 @@ public class CookBookDriver implements Serializable
         {
             type = "cooking";
         }
-        System.out.println("Please enter the title for this " + type + " recipe: ");
+        System.out.print(" \n" + "Please enter the title for this " + type + " recipe: ");
         if(inScan.hasNextLine())
         {
             newTitle = inScan.nextLine();
@@ -221,13 +309,118 @@ public class CookBookDriver implements Serializable
                 currentRecipes.add(newRecipeBatch.get(index));
                 numRecipes++;
             }
+            System.out.println("Changes only apply to current recipes list."
+            + "\nTo keep changes after exit, please save current recipes list.");
         }
         else
         {
             System.out.println("There are no loaded recipes to merge.");
         }
-        System.out.println("Changes only apply to current recipes list."
-            + "\nAny saved or loaded files are unchanged.");
+    }
+    
+    /**search for recipe in current recipes list by title
+     * @param recipeTitle (Str) - title of desired recipe
+     * @return boolean - true if recipe found
+     */
+    public boolean recipeInCurrentRecipes(String recipeTitle)
+    {
+        for(int index = 0; index < currentRecipes.size(); index++)
+        {
+            if(currentRecipes.get(index).getTitle().toLowerCase().equals(recipeTitle))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**search for recipe in new recipe batch list by title
+     * @param recipeTitle (Str) - title of desired recipe
+     * @return boolean - true if recipe found
+     */
+    public boolean recipeInNewRecipeBatch(String recipeTitle)
+    {
+        for(int index = 0; index < newRecipeBatch.size(); index++)
+        {
+            if(newRecipeBatch.get(index).getTitle().toLowerCase().equals(recipeTitle))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**search for recipe by title and display recipe details (menu option 7)
+     * 
+     */
+    public void findRecipeByTitle()
+    {
+        String title = getTitleInput();
+        if(!currentRecipes.isEmpty() && recipeInCurrentRecipes(title))
+        {
+            System.out.println("Found recipe in current recipe list:");
+            displayRecipe(currentRecipes, title);
+        }
+        else if(!newRecipeBatch.isEmpty() && recipeInNewRecipeBatch(title))
+        {
+            System.out.println("Found recipe in loaded recipes list:");
+            displayRecipe(newRecipeBatch, title);
+        }
+        else
+        {
+            System.out.println("Sorry, no recipe by that title exists");
+        }
+    }
+    
+    /**get recipe title input
+     * @return recipeTitle (Str) - lowercase recipe title
+     */
+    public String getTitleInput()
+    {
+        String recipeTitle = "";
+        System.out.print("Please enter the title of the desired recipe: ");
+        if(inScan.hasNextLine())
+        {
+            recipeTitle = inScan.nextLine();
+        }
+        return recipeTitle.toLowerCase();
+    }
+    
+    /** delete recipe from recipe list
+     * @param recipeList (ArrayList<BasicRecipe>) - list containing desired recipe
+     * @param recipeTitle (Str) - title of desired recipe
+     */
+    public void deleteRecipe(ArrayList<BasicRecipe> recipeList, String recipeTitle)
+    {
+        for(int index = 0; index < recipeList.size(); index++)
+        {
+            if(recipeList.get(index).getTitle().toLowerCase().equals(recipeTitle))
+            {
+                recipeList.remove(recipeList.get(index));
+            }
+        }
+    }
+    
+    /**find recipe by title and delete (menu option 8)
+     * 
+     */
+    public void findAndDeleteRecipe()
+    {
+        String title = getTitleInput();
+        if(!currentRecipes.isEmpty() && recipeInCurrentRecipes(title))
+        {
+            System.out.println("Deleting recipe from current recipe list...");
+            deleteRecipe(currentRecipes, title);
+        }
+        else if(!newRecipeBatch.isEmpty() && recipeInNewRecipeBatch(title))
+        {
+            System.out.println("Deleting recipe from loaded recipes list...");
+            deleteRecipe(newRecipeBatch, title);
+        }
+        else
+        {
+            System.out.println("Sorry, no recipe by that title exists");
+        }
     }
     
     //String version of read recipes from file idea, unused, possibly obsolete
@@ -310,7 +503,7 @@ public class CookBookDriver implements Serializable
         }
     }
     
-    //recipe display method:
+    //recipe display methods:
     /** format and display recipe details (menu options 2 & 5)
      * @param recipeArr (ArrayList<BasicRecipe>) - recipes to print
      */
@@ -333,6 +526,30 @@ public class CookBookDriver implements Serializable
         }
     }
     
+    /**display one recipe
+     * @param recipeList (ArrayList<BasicRecipe>) - recipe list with desired recipe
+     * @param recipeTitle (Str) - title of desired recipe
+     */
+    public void displayRecipe(ArrayList<BasicRecipe> recipeList, String recipeTitle)
+    {
+        String recipeDetails = "";
+        BasicRecipe recipe = null;
+        for(int index = 0; index < recipeList.size(); index++)
+        {
+            if(recipeList.get(index).getTitle().toLowerCase().equals(recipeTitle))
+            {
+                recipe = recipeList.get(index);
+            }
+        }
+        ArrayList<Ingredient> ingredients = recipe.getIngredients();
+        for(int i = 0; i < ingredients.size(); i++)
+        {
+            recipeDetails += "       " + ingredients.get(i).getName() + ":"
+                                + ingredients.get(i).getAmount() + "\n";
+        }
+        System.out.println(recipe.getTitle() + ": \n" + "   Ingredients: \n"
+            + recipeDetails + "\n   " + recipe.getCookingTime());
+    }
     
     // Read & Write ArrayList recipe data file methods: added Apr 27, 2022
     /** get recipe ArrayList from file
@@ -419,6 +636,10 @@ public class CookBookDriver implements Serializable
             {
                 invalid = false;
             }
+            else
+            {
+                System.out.println("Filename must include '.bin'.");
+            }
         }
         while(invalid);
         return newFilename;
@@ -440,6 +661,8 @@ public class CookBookDriver implements Serializable
                 System.out.println("Creating a recipe!");
                 c.getChoiceSetNewRecipe(); // get recipe type, create new recipe
                 c.setNewTitle(); // add recipe title
+                c.addIngredients(); //add ingredient(s) to recipe
+                c.setCookTime(); // get input, set cook time for recipe
             }
             else if(c.menuChoice.equals("2")) // view current recipes
             {
@@ -474,6 +697,30 @@ public class CookBookDriver implements Serializable
             else if(c.menuChoice.equals("6")) // merge current & loaded recipe lists
             {
                 c.mergeRecipeLists(); // add loaded recipes to current recipe list
+            }
+            else if(c.menuChoice.equals("7"))
+            {
+                if((c.currentRecipes != null && c.currentRecipes.size() > 0) || 
+                        (c.newRecipeBatch != null && c.newRecipeBatch.size() > 0))
+                {
+                    c.findRecipeByTitle(); // get title, search lists, display recipe
+                }
+                else
+                {
+                    System.out.println("There are no recipes to search");
+                }
+            }
+            else if(c.menuChoice.equals("8"))
+            {
+                if((c.currentRecipes != null && c.currentRecipes.size() >0) ||
+                        (c.newRecipeBatch != null && c.newRecipeBatch.size() > 0))
+                {
+                    c.findAndDeleteRecipe(); // get title, search lists, delete recipe
+                }
+                else
+                {
+                    System.out.println("There are no recipes to search");
+                }
             }
         }
         System.out.println("Goodbye!");
